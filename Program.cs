a0debfcb032a861fd96bc211adb8cea1579d48a5
Eduardo Portella <eportella70@gmail.com>
2024-03-18 22:35:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.ObjectModel;
+
+Console.WriteLine("Hello, worlddd!");
+
+int i;
+int i2 = i = 10;
+
+i = i2 == 42 ? 10 : 20;
+
+Console.WriteLine(i2--);
+
