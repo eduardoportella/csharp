93e36f36cf000f694091ccea96806379b3ccf606
Eduardo Portella <eportella70@gmail.com>
2024-03-19 22:10:18 -0300

    getters e setters

--- Program.cs
+++ Program.cs
@@ -3,31 +3,49 @@
 class Program
 {
    static void Main(){
-      // string s = null;
-      int[] numbers = [1];
+      BankAccount acc1 = new BankAccount("Eduardo", 1000);
+      BankAccount acc2 = new BankAccount("Larissa", 999);
 
+      // acc1.Deposit(-100);
+      acc2.Deposit(200);
 
-      try
-      {
-         Console.WriteLine(numbers[1]);
-   
-      }
-      // catch (System.Exception)
-      catch (System.NullReferenceException exception)
-      {
-         Console.WriteLine($"erro de referência nula. {exception.Message}");
-      }
-
-      catch (System.IndexOutOfRangeException exception)
-      {
-         Console.WriteLine($"erro. {exception.Message}");
-      }
-
-      catch (Exception exception){
-         Console.WriteLine($"Erro: {exception.Message}");
-      }
-
-
-
+      Console.WriteLine(acc2.Balance);
    }
+}
+
+class BankAccount
+{
+   private string name;
+
+   public decimal Balance {
+      get; private set;
+      // get {return balance; } 
+      // private set {
+      //    if (value >= 0 ) {
+      //       balance = value; 
+      //    } else {
+      //       // ...
+      //    }
+      // } 
+   }
+
+   public BankAccount(string name, decimal balance){
+      if (string.IsNullOrWhiteSpace(name)){
+         // throw new Exception("Nome inválido.");
+         throw new ArgumentException("Nome inválido.", nameof(name));
+      }
+      if (balance < 0){
+         throw new Exception("Saldo não pode ser negativo");
+      }
+      this.name = name;
+      Balance = balance;
+   }
+
+   public void Deposit(decimal amount){
+      if (amount <= 0){
+         throw new ArgumentException("Depósito não pode ser menor ou igual a zero.", nameof(amount));
+      }
+      Balance += amount;
+   }
+
 }
