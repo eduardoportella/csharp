76a2ac18b33b644e03f5c197d9c58c3a102c5368
Eduardo Portella <eportella70@gmail.com>
2024-03-19 21:37:20 -0300

    tratamentos de excecao

--- Program.cs
+++ Program.cs
@@ -3,63 +3,31 @@
 class Program
 {
    static void Main(){
+      // string s = null;
+      int[] numbers = [1];
+
+
+      try
+      {
+         Console.WriteLine(numbers[1]);
    
+      }
+      // catch (System.Exception)
+      catch (System.NullReferenceException exception)
+      {
+         Console.WriteLine($"erro de referência nula. {exception.Message}");
+      }
 
-   // int i = 10;
+      catch (System.IndexOutOfRangeException exception)
+      {
+         Console.WriteLine($"erro. {exception.Message}");
+      }
 
-   // if (int.TryParse("asdfadsf", out int result)){
-   //    Console.WriteLine("sucesso");
-   // } else {
-   //    Console.WriteLine("false");
-   // }
-   
-   // Console.WriteLine(result);
-
-   // int i = 10;
-   // long l = 10;
+      catch (Exception exception){
+         Console.WriteLine($"Erro: {exception.Message}");
+      }
 
 
-   // //Long pode armazenar inteiro
-   // l = i;
-
-   // //Int nao pode receber devido a memoria
-   // // i = l;
-
-   // i = (int)l; //Conversao explicita
-   // string s = l.ToString();
-
-   // Console.WriteLine(i);
-
-   int? i = 10;
-   // int i2 = i;
-   i = 30;
-   // i2 = 20;
-
-   // i = null;
-
-   // i.HasValue;
-   // i.Value;
-   // i.GetValueOrDefault();
-
-   if (i is not null) {
-      Console.WriteLine("nulo");
-   }
-
-   // Test t = new Test();
-   // t.X = 12;
-
-   // // Test t2 = t; //Aqui eh referencia pra t
-   // Test t2 = new Test();
-   // t2.X = 20;
-
-   // Console.WriteLine(t.X);
 
    }
 }
-
-class Test{
-
-   public int X;
-
-}
-
